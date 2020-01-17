7d691654a353fb6f63a93be3f1ace68d2686c48f
Stefan Krug <stefan.krug@posteo.de>
2020-01-17 11:49:38 +0100

    getHistory.sh parametrisiert

--- script/getHistory.sh
+++ script/getHistory.sh
@@ -1,49 +1,87 @@
 #!/bin/bash
 #
 # Das Skript ruft eine angegebene Wikipedia-Artikelgeschichte ab und speichert neben der unveränderten HTML-Datei auch eine auf Kennwerte reduzierte und vom HTML freigestellte XML.
-# Für den zweiten Schritt ist es notwendig, dass sich die Transformationsdatei >history.xsl< im Skriptverzeichnis befindet.
+# Für den zweiten Schritt ist es notwendig, dass sich die Transformationsdatei (i.d.R. history.xsl) im Skriptverzeichnis befindet.
+#
+# Parameter:
+#			-u 		(optional) Abzurufende URL. Jede URL ist valide, die Transformation ist jedoch nur auf die Versionsgeschichten der Wikipedia ausgelegt. Bei fehlender Eingabe wird URL zur Laufzeit abgefragt.
+#			-v		(optional) Arbeitsverzeichnis. Legt einen definierten Unterordner an und speichert dort die zu erzeugenden Dateien.
 #
 # Autor: 	Stefan Krug
-# Stand:	2020-01-09
+# Stand:	2020-01-17
 
-echo "\n### getHistory.sh - Stand 2020-01-09 - Initialisierung.."
+echo "\n### getHistory.sh - Stand 2020-01-17 - Initialisierung.."
 
-## Parameterdefinition
-stylesheet="history.xsl"		# XSLT, muss für Transformation vorhanden sein
-htmlFile="historyData.html"		# Zieldatei für das HTML-Abbild
-xmlFile="historyData.xml"		# Zieldatei für die XML-Datei
-logFile="historyData_log.txt"	# Zieldatei für das Log
-url=""						# URL zur abzurufenden Versionsgeschichte
+## Variablendefinition
+	stylesheet="history.xsl"		# XSLT, muss für Transformation vorhanden sein
+	htmlFile="historyData.html"		# Zieldatei für das HTML-Abbild
+	xmlFile="historyData.xml"		# Zieldatei für die XML-Datei
+	logFile="historyData_log.txt"	# Zieldatei für das Log
+	verzeichnis="false"			# Arbeitsverzeichnis für die zu erstellenden Dateien
+	url="false"					# URL zur abzurufenden Versionsgeschichte
+
+## Parameter übergeben
+	while getopts u:v: option; do
+		case "${option}" in
+			
+			u) url=${OPTARG};;
+			v) verzeichnis=${OPTARG};;
+		
+		esac
+	done
+	
+## Pfadvariablen anpassen und Arbeitsverzeichnis anlegen - sofern angegeben
+	if [ $verzeichnis != "false" ]; then
+		
+		mkdir -p ./$verzeichnis
+		htmlFile=$verzeichnis/$htmlFile
+		xmlFile=$verzeichnis/$xmlFile
+		logFile=$verzeichnis/$logFile
+	fi
 
 uname -s -r -v -m >> $logFile	# initialer logFile-Eintrag mit Systeminformationen
 echo $(date) "getHistory.sh - start" >> $logFile
+echo "definiertes Arbeitsverzeichnis=" $verzeichnis
 
-## 1. Prüfung auf history.xsl
-if [ -f "$stylesheet" ];then
-	echo "Schemadatei" $stylesheet "gefunden."
-else
-	echo "Schemadatei" $stylesheet "wurde nicht gefunden. Transformationsfunktion deaktiviert."
-	stylesheet="false"
-fi
+## Prüfung auf Transformationsschema - ohne wird kein Transformationsversuch unternommen
+	if [ -f "$stylesheet" ]; then
 
-## 2. Eingabeaufforderung für URL
-echo "\nBitte fuegen Sie die parametrisierte URL der zu verarbeitenden Wikipedia-Versionsgeschichte ein. Achten Sie auf eine sinnvolle Groesse des Parameters &limit.\n"
-read url
-echo "\nDie URL \""$url"\" wird verarbeitet.."
+		echo "-> Schemadatei" $stylesheet "gefunden." >> $logFile
+	
+	else
+		
+		echo "-> Schemadatei" $stylesheet "wurde nicht gefunden." >> $logFile
+		stylesheet="false"
+		
+	fi
+
+## Prüfung auf URL - wenn false, wird eine Eingabe verlangt
+	if [ $url = "false" ]; then
+	
+		echo "\nBitte fuegen Sie die parametrisierte URL der zu verarbeitenden Wikipedia-Versionsgeschichte ein. Achten Sie auf eine sinnvolle Groesse des Parameters &limit.\n"
+		read url
+	
+	fi
+	
 echo "-> URL zur Verarbeitung:" $url "\n" >> $logFile
 
-# 3. Abruf der HTML via wget ("-a" append logfile, "-O" Zieldatei)
-wget -a $logFile -O $htmlFile $url
+## Abruf der HTML via wget ("-a" append logfile, "-O" Zieldatei)
+	
+	wget -a $logFile -O $htmlFile $url
 
 echo "\nDas HTML-Abbild wurde als" $htmlFile "gespeichert."
 
-# 4. transformation via xsltproc (if 1.)
-if [ $stylesheet != "false" ]; then
-	echo "XSLT wird angewendet.."
-	echo "-> xsltproc wird ausgefuehrt" >> $logFile
-	xsltproc -v -o $xmlFile $stylesheet $htmlFile
-	echo "\nBerenigte Daten wurden als" $xmlFile "gespeichert."
-	echo "-> Auswertung wurde nach" $xmlFile "geschrieben." >> $logFile
-fi
+## Transformation via xsltproc und Schemadatei
+	if [ $stylesheet != "false" ]; then
+
+		echo "XSLT wird angewendet.."
+		echo "-> Schemadatei wird mittels xsltproc angewendet" >> $logFile
+		
+		xsltproc -v -o $xmlFile $stylesheet $htmlFile
+		
+		echo "\nBerenigte Daten wurden als" $xmlFile "gespeichert."
+		echo "-> Auswertung wurde nach" $xmlFile "geschrieben." >> $logFile
+	
+	fi
 
 echo $(date) "getHistory.sh - end\n" >> $logFile
