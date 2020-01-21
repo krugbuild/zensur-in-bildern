4e6f5bf82bde6405424c0dc98dd7dcaf44b8e400
Stefan Krug <stefan.krug@posteo.de>
2020-01-21 17:40:09 +0100

    getImageTable.sh hinzugefuegt

--- script/getImageTable.sh
+++ script/getImageTable.sh
@@ -10,7 +10,7 @@ imageList=""
 
 echo "\n### getImageTable.sh - Stand 2020-01-21 - Initialisierung.."
 
-	# Bild-ID-Liste aus unique images erzeugen
+	# Bild-ID-Liste aus einzigartigen Image-URL erzeugen
 	imageList=$(eval 'xmllint $imagesXML -xpath "article/version/image[not(url = preceding:: url)]/@id"')
 	
 	if [ -f $htmlFile ]; then	# es folgen Append-Operationen, deshalb brauchen wir eine 'leere' Datei
@@ -25,12 +25,13 @@ echo "\n### getImageTable.sh - Stand 2020-01-21 - Initialisierung.."
 	# Table öffnen und erste Zelle (a:1) leer lassen
 	echo "<table><tr><th/>" >> $htmlFile
 	
-	# Überschriften in erste Zeile schreiben
+	# Bilder in erste Zeile schreiben
 	#for url in $(eval 'xmllint $imagesXML -xpath "article/version/image[not(url = preceding:: url)]/@id"')
 	for image in $imageList
 	do
 
-		url=$(eval 'xmllint $imagesXML -xpath "article/version[1]/image[@$image]/url/text()"')
+		url=$(eval 'xmllint $imagesXML -xpath "(article/version/image[@$image]/url/text())[1]"')
+		
 		echo "<th><img width='150' src='https:$url'/></th>" >> $htmlFile
 		
 	done 
@@ -48,12 +49,11 @@ echo "\n### getImageTable.sh - Stand 2020-01-21 - Initialisierung.."
 		# Prüfung, welche Bild-ID in dieser Version vorkommen
 		for id in $imageList;
 		do
-			
-			#key=$(eval 'xmllint $imagesXML -xpath "article/version/image[@$id]/url/text()"')
-			#echo $key
-			
+			# URL des Vergleichsbildes als uid auslesen (Text des ersten Nodes aus dem Ergebnisset)
+			urlVergleich=$(eval 'xmllint $imagesXML -xpath "(article/version/image[@$id]/url/text())[1]"')
+			echo "Pruefung gegen" $urlVergleich
 			# Prüfung auf Version-ID und Image-ID - bei Erfolg wird ID ausgegeben
-			echo "<td>"$(eval 'xmllint $imagesXML -xpath "article/version[@$version]/image[@$id]/@id"')"</td>" >> $htmlFile
+			echo "<td>"$(eval 'xmllint $imagesXML -xpath "article/version[@$version]/image[url=\"$urlVergleich\"]/@id"')"</td>" >> $htmlFile
 		
 		done 
 	
